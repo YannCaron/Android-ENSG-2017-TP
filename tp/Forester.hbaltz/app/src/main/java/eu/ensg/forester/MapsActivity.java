11378530d4c276d5d6f56eae219e15703cc34a40
hbaltz <hugo.baltz@ensg.eu>
2017-02-01 14:59:07 +0100

    Affichage des secteurs

--- tp/Forester.hbaltz/app/src/main/java/eu/ensg/forester/MapsActivity.java
+++ tp/Forester.hbaltz/app/src/main/java/eu/ensg/forester/MapsActivity.java
@@ -1,6 +1,7 @@
 package eu.ensg.forester;
 
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -26,9 +27,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import eu.ensg.spatialite.geom.Point;
 import eu.ensg.spatialite.geom.Polygon;
+import eu.ensg.spatialite.geom.XY;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -37,14 +40,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     // Gestion de la position
     private Point pos;
     private TextView lblPosition;
+    private LocationManager locMgr;
+    private GpsListener gpsListener;
 
     // Gestion des polygones
     private Polygon currentDistrict;
-    private boolean isRecording = false;
-
-    private Button btSave;
-    private Button btAbort;
+    private boolean isRecording;
     private LinearLayout llRecording;
+    private PolygonOptions polyOptions;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
@@ -56,7 +59,26 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // Manages location
+        locMgr = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        gpsListener = new GpsListener();
+
         manageViews();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locMgr.removeUpdates(gpsListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startListenerLoc();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -94,10 +116,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LocationManager locMgr = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-
         Criteria criteria = new Criteria();
         String provider = locMgr.getBestProvider(criteria, false);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
         Location location = locMgr.getLastKnownLocation(provider);
 
         if (location != null) {
@@ -112,15 +136,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d("Provider", "Provider not available");
         }
 
-        // Define which provider the application will use regarding which one is available
-        if (locMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            locMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, (android.location.LocationListener) new GpsListener());
-        } else {
-            locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, (android.location.LocationListener) new GpsListener());
-        }
+        startListenerLoc();
 
         // Add a marker in Sydney and move the camera
         LatLng paris = new LatLng(48, 2);
@@ -168,14 +184,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         lblPosition = (TextView) findViewById(R.id.lblPosition);
 
         llRecording = (LinearLayout) findViewById(R.id.recordingMenu);
-        llRecording.setVisibility(View.GONE);
+        activateRecording(false);
 
-        btSave = (Button) findViewById(R.id.btSave);
-        btAbort = (Button) findViewById(R.id.btAbort);
+        Button btSave = (Button) findViewById(R.id.btSave);
+        Button btAbort = (Button) findViewById(R.id.btAbort);
 
         btSave.setText(getString(R.string.sv));
         btAbort.setText(getString(R.string.abrt));
 
+        btSave.setOnClickListener(new saveListener());
+        btAbort.setOnClickListener(new AbortListener());
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -194,6 +212,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if(isRecording){
             currentDistrict.addCoordinate((float)pos.getCoordinate().getX(),(float)pos.getCoordinate().getY());
+
+            polyOptions.add(new LatLng(pos.getCoordinate().getY(),pos.getCoordinate().getX()));
+
+            mMap.clear();
+            mMap.addPolygon(polyOptions);
         }
 
         popToast("lat : " + pos.getCoordinate().getY() + ", long : " + pos.getCoordinate().getX(), true);
@@ -228,6 +251,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * Creates a poi on the map
      */
     private void createPt(){
+        mMap.clear();
         mMap.setOnMapClickListener(new MapClickListenerPt());
     }
 
@@ -240,9 +264,58 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Creates a sector
+     */
     private void createSt(){
-        isRecording = true;
+        mMap.clear();
         currentDistrict = new Polygon();
-        llRecording.setVisibility(View.VISIBLE);
+        polyOptions = new PolygonOptions();
+        polyOptions.strokeColor(Color.RED)
+                .fillColor(Color.BLUE);
+        activateRecording(true);
+    }
+
+    private class saveListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            popToast("Save", true);
+            activateRecording(false);
+        }
+    }
+
+    private class AbortListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            popToast("Abort", true);
+            activateRecording(false);
+        }
+    }
+
+    /**
+     * Activates or deactivates the recording
+     * @param bool true activates the recording, false deactivates it
+     */
+    private void activateRecording(boolean bool){
+        isRecording = bool;
+        if(bool){
+            llRecording.setVisibility(View.VISIBLE);
+        }else{
+            llRecording.setVisibility(View.GONE);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void startListenerLoc(){
+        // Define which provider the application will use regarding which one is available
+        if (locMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsListener);
+        } else {
+            locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsListener);
+        }
     }
 }
